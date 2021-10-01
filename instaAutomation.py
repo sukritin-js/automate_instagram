24092f01d388a784796487eea7bfa91e67d32047
sukritin <72253561+sukritin-js@users.noreply.github.com>
2021-10-01 12:12:51 +0530

    Create instaAutomation.py
    File name will able to change but 
    Tasks required is important

---
+++ instaAutomation.py
@@ -0,0 +1,7 @@
+# how to make a python program to automate the instagram
+# Tasks Required
+# 1> If a celebs upload a pic then which person liked that photo our bot will follow him
+# 2> make sure the password is not shown in the python file 
+# 3> use time module to implement the time duration
+# -------------------------------------------------------------
+# That's it will give you and pull requests 
